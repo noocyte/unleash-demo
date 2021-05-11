712bb0d2483d9c90f5000b50a83ebc671a00244a
Jarle Nygård <jarle.nygaard@outlook.com>
2021-05-11 10:37:19 +0200

    Client calling in loop

--- api/Startup.cs
+++ api/Startup.cs
@@ -1,16 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 
 namespace unleash_demo
 {
@@ -28,10 +20,6 @@ namespace unleash_demo
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "unleash_demo", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,15 +28,9 @@ namespace unleash_demo
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "unleash_demo v1"));
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
--- client/Program.cs
+++ client/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var rnd = new Random();
+            var client = new HttpClient();
+            var address = "http://localhost:5000/Sum?";
+            do
+            {
+                try
+                {
+                    var num1 = rnd.Next(0, 10);
+                    var num2 = rnd.Next(0, 10);
+                    var finalAddress = $"{address}num1={num1}&num2={num2}";
+                    var response = await client.GetAsync(finalAddress);
+                    var content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Sum: {content}");
+
+
+                    await Task.Delay(1500);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Failed - waiting to retry");
+                    await Task.Delay(2000);
+                }
+               
+            } while (true);
         }
     }
 }
